7077781dcba62d663a9375ad61cc87f375a44db2
FonWasH <fonwash@gmail.com>
2019-08-16 00:43:50 +0200

    init

---
+++ esx_procurer/locales/fr.lua
@@ -0,0 +1,9 @@
+Locales['fr'] = {
+  ['press_to_guard'] = 'appuyez sur ~INPUT_CONTEXT~ ~o~pour protéger la prostitué.',
+  ['work_timer'] = 'il reste ~r~%s~s~ secondes',
+  ['recently_worked'] = 'Elle a déjà bosser. Laisse la se reposer ~r~%s~b~ secondes.',
+  ['started_to_guard'] = 'vous avez commencé à la protection.',
+  ['guard_complete'] = '~r~Travail terminé~s~. Vous avez ~o~gagné~s~ ~g~$%s~s~',
+  ['guard_cancelled'] = 'restez à coté d\'elle pour la protéger sinon vous ne gagnerez rien!',
+  ['working_already'] = '~r~Quelqu\'un d\'autre est déjà sur le coup! Occupez vous-en ou barrez vous!',
+}
